166243dac2d92efd4a6bc38774c0f4315007f3c8
Tarek Ziadé <tarek@ziade.org>
2023-04-17 22:30:57 -0700

    added Pysos

--- README.md
+++ README.md
@@ -7,6 +7,8 @@ Implementations:
 
 - dbm
 - sqlite3
+- pysos (https://github.com/dagnelies/pysos)
+
 
 dbm has faster lookups!
 
@@ -37,5 +39,18 @@ max id len is 10
 Looking for one id
 0.012021810980513692 seconds
 Size of DB on disk is 54.109375 MiB
+```
+
+## Pysis benchmark
+
+Maximum RSS: 300MiB
+
+```sh
+Adding 1 million ids and timestamps
+took 15.620530843734741 seconds
+Final count 1000000
+max id len is 10
+Looking for one id
+0.0018717409984674305 seconds
 ```
 
---
+++ pysob.py
@@ -0,0 +1,45 @@
+# 1 Million ids
+import os
+import pysos
+import hashlib
+import time
+from datetime import datetime, timezone
+import timeit
+
+
+DB_NAME = "pysos.db"
+
+if os.path.exists(DB_NAME):
+    print(f"DB exists, delete {DB_NAME} to recreate")
+else:
+    db = pysos.Dict(DB_NAME)
+
+    s = time.time()
+    print("Adding 1 million ids and timestamps")
+    max_len = 0
+
+    for i in range(1000):
+        when = str(datetime.now(timezone.utc).timestamp())
+        for y in range(1000):
+            id = f"{i}0000{y}"
+            if len(id) > max_len:
+                max_len = len(id)
+            db[id] = str(when)
+
+    print(f"took {time.time() - s} seconds")
+    print(f"Final count {len(db)}")
+    print(f"max id len is {max_len}")
+    db.close()
+
+db = pysos.Dict(DB_NAME)
+print("Looking for one id")
+
+
+def _lookup():
+    assert db["23000034"] is not None
+
+
+print(f"{timeit.timeit(_lookup, number=1000)} seconds")
+
+
+print(f"Size of DB on disk is {os.stat(DB_NAME).st_size / 1024. / 1024.} MiB")
